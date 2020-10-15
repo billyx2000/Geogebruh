e06e0db023df30059ebe915b92df95c50bc96c0c
Milan6959 <milan0169@hotmail.com>
2020-10-15 16:17:01 +0200

    Fenêtre graphique et fonctionnalités

--- test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from tkinter import *
-x=np.linspace(-19,14,100000)
-plt.plot(x,8*np.sin(x))  # on utilise la fonction sinus de Numpy
-plt.ylabel('fonction 8*sinus')
-plt.xlabel("l'axe des abcisses")
-plt.show()
