d069394fd19897e7de07f6776e149089f7006349
Milan6959 <milan0169@hotmail.com>
2020-12-10 13:26:54 +0100

    Fixed power function issue

--- src/lib_fct.py
+++ src/lib_fct.py
@@ -20,71 +20,70 @@ class UsualFct(Enum):
 
 # Function : EVALUATEUR
 def VerifToken(tokenArray):
-
-    # DEBUG
-    #print(tokenArray)
-
     tokenArray = tokenArray.asList()
-    tokenString = ''.join(str(v) for v in tokenArray).replace('[', '').replace(']', '') #List to string and replace '[' to '('
+    tokenString = ''.join(str(v) for v in tokenArray).replace("]", '').replace("[", '') #List to string and remove '[' and ']'
 
-    # DEBUG
-    #print(tokenString)
+    tokenArray = re.compile("'([^\'])'").split(tokenString)  #Grabbing value between simple quote
+    tokenString = ""
+    for element in tokenArray:
+        element = re.sub(r'(\w+),(\w+)', r'\1_\2', element)  #Fix Power issue, replacing ',' to '_' in array element
+        tokenString += element  #Convert to string 
 
+    tokenString = tokenString.replace(",", '').replace(" ", '').replace("'", '')  #removing withespace and quote
+    tokenString = tokenString.replace("_", ',')  #replacing '_' to simple ','
+    
     # Match all funtion string, with a length from 3 to 6
     tokenFct = re.findall(r'\b[a-zA-Z]{3,6}',tokenString)
 
     # Check if given function(s) are supported
     for fct in tokenFct:
         if fct not in UsualFct._member_names_:
-
             # DEBUG
             #print("Mauvaise saisie: ")
             #print(fct)
-
             return False
 
         else:
             # Prefix function name with "np" to use numpy math function (function_name => np.function_name)
             tokenString = tokenString.replace(fct, "np."+fct)
 
-    # Clean up the token string
-    tokenString = tokenString.replace(",", '')
-    tokenString = tokenString.replace(" ", '')
-    tokenString = tokenString.replace("'", '')
-    # Regex fix to power(x, y) issue (above lines replace the comma, that is needed for this function)
-    tokenString = re.sub(r'(.*\(x)(\d\))', r'\1,\2', tokenString)
-
     return tokenString
+
 
 # Function : ANALYSEUR LEXICAL
 def LexicalAnalysis(myExpr):
     expr = Forward()
-    double = Word(nums + ".").setParseAction(lambda t:float(t[0]))
-    integer = Word(nums).setParseAction(lambda t:int(t[0]))
-    variable = Word(alphas)
-    string = dblQuotedString
-    funccall = Group(variable + "(" + Group(Optional(delimitedList(expr))) + ")")
-    array_func = Group(funccall + "[" + Group(delimitedList(expr, "][")) + "]")
-    array_var = Group(variable + "[" + Group(delimitedList(expr, "][")) + "]")
+    integer = Word(nums).setParseAction(lambda t:int(t[0]))  #  -> "W:(0-9)"
+    variable = Word(alphas)    ##     -> "W:(A-Za-z)"
+  
+    # define a key_value pair using Group to preserve structure
+    argFunc = Group(Optional(delimitedList(expr, delim=',', combine=True)))
+    funccall = Group(variable + "(" + argFunc + ")")
+    
+    operand = integer | funccall | variable
 
-    operand = integer | string | array_func | funccall | array_var | variable
-
-    expop = Literal('^')
-    signop = oneOf('+ -')
-    multop = oneOf('* /')
-    plusop = oneOf('+ -')
-
-    expr << operatorPrecedence( operand,
-    [("^", 2, opAssoc.RIGHT),
-    (signop, 1, opAssoc.RIGHT),
-    (multop, 2, opAssoc.LEFT),
-    (plusop, 2, opAssoc.LEFT),]
-    )
-
+    expr << infixNotation( operand,
+     [
+        ('-', 1, opAssoc.RIGHT),
+        (oneOf('* /'), 2, opAssoc.LEFT),  #Repetition of one or more of the given expression
+        (oneOf('+ -'), 2, opAssoc.LEFT),
+    ])
+    
     result = expr.parseString(myExpr)
+    
+   # DEBUG
+   # expr.runTests('''
+   # -2--11
+   # 5+3*6
+   # (5+3)*6
+   # cos(x+1) + 20
+   # power(cos(x+1)*x, 4)
+   # power(x,5)
+   # ''')
+    
     return(result)
 
 # DEBUG
-#print(VerifToken(LexicalAnalysis('2*sin(t)')))
-#print(VerifToken(LexicalAnalysis('power(x,5)')))
+#VerifToken(LexicalAnalysis('power(cos(x+1)*x, 4)'))
+#VerifToken(LexicalAnalysis('power(x, 4)+5'))
 
